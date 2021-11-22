5a85a5aa1f785af9525b847fd97bc8bd12b5b6f5
onepullnixxa <briseno.chris@gmail.com>
2021-11-22 13:53:07 -0500

    adds log out button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 import Main from "./components/Main";
 
 function App() {
   return (
     <div>
       <LoginButton />
+      <LogoutButton />
       <Header />
       <Main />
 
